a036056dc574db3350e2d8d1cf0ec1d0067b2bc2
f-kubotar <dev@hadashikick.jp>
2015-08-09 20:37:17 +0900

    impl waveclock

--- src/client/index.js
+++ src/client/index.js
@@ -13,92 +13,71 @@ import 'processing-js';
   window.processing = new Processing('stage', sketch);
 });
 
-function customRandom(p) {
-  return 1 - p.pow(p.random(1), 2);
-};
-
-function createPoints(p, centerX, centerY, baseRadius) {
-  let radiusNoise = p.random(10);
-  let points = [];
-  
-  // for (let angle = 0; angle <= 14400; angle += 5) {
-  for (let angle = 0; angle <= 2880; angle += 5) {
-    // radiusNoise += 0.05;
-    let r = baseRadius + (p.noise(radiusNoise + angle) * 200) - 100;
-    
-    let rad = p.radians(angle);
-    let x = centerX + p.cos(rad) * r;
-    let y = centerY + p.sin(rad) * r;
-    points.push({ x: x, y: y});
-  }
-  return points;
+function drawPoint(p, x, y, noiseFactor) {
+  const len = 10 * noiseFactor;
+  p.rect(x, y, len, len);
 }
 
 function sketch(p) {
   window.p = p;
+
+  const h = 300;
+  const w = 300;
+
+  let strokeColorStep = -1;
+  let strokeColor = 254;
   
-  let w = 500;
-  let h = 500;
-  let diam = 10;
-  let cx = w * 0.5;
-  let cy = h * 0.5;
-
-  let radius = 100;
-
-  let points     = createPoints(p, cx, cy, radius);
-  let nextPoints = createPoints(p, cx, cy, radius);
-
-  let progress = 0;
-
+  let angle = -Math.PI * 0.5;
+  let angNoise, radiusNoise, xNoise, yNoise;
+  
   p.setup = () => {
-    p.frameRate(24);
     p.size(w, h);
     p.smooth();
     p.background(255);
-
-    p.stroke(0, 30);
-    p.strokeWeight(0.5);
     p.noFill();
-    p.ellipse(cx, cy, radius * 2, radius * 2);
+    p.strokeWeight(1);
 
-    p.stroke(20, 50, 70);
+    angNoise    = p.random(10);
+    radiusNoise = p.random(10);
 
-    // let r2 = 10;
-    let points = createPoints(p, cx, cy, radius);
-    let length = points.length;
-    for (let i = 1; i < length; i++) {
-      let p1 = points[i - 1];
-      let p2 = points[i];
-      p.line(p1.x, p1.y, p2.x, p2.y);
-    }
+    xNoise = p.random(10);
+    yNoise = p.random(10);
   };
 
   p.draw = () => {
-    p.background(255);
+    radiusNoise += 0.005;
+    let radius = p.noise(radiusNoise) * 550 + 1;
 
-    if (progress >= 1) {
-      points     = nextPoints;
-      nextPoints = createPoints(p, cx, cy, radius);
-      progress = 0;
+    angNoise += 0.005;
+    angle += p.noise(angNoise) * 6 - 3;
+
+    if (angle > 360) {
+      angle -= 360;
+    }
+    if (angle < 0) {
+      angle += 360;
     }
 
-    let length = points.length;
-    for (let i = 1; i < length; i++) {
-      let p1 = points[i - 1];
-      let p2 = points[i];
-
-      let np1 = nextPoints[i - 1];
-      let np2 = nextPoints[i];
-      
-      let x1 = p1.x + ((np1.x - p1.x)  * (progress));
-      let y1 = p1.y + ((np1.y - p1.y) * (progress));
-      let x2 = p2.x + ((np2.x - p2.x)  * (progress));
-      let y2 = p2.y + ((np2.y - p2.y) * (progress));
-     
-      p.line(x1, y1, x2, y2);
+    xNoise += 0.01;
+    yNoise += 0.01;
+    let centerX = w * 0.5 + (p.noise(xNoise) * 100) - 50;
+    let centerY = h * 0.5 + (p.noise(yNoise) * 100) - 50;
+    let rad = p.radians(angle);
+    let x1 = centerX + (radius * p.cos(rad));
+    let y1 = centerY + (radius * p.sin(rad));
+    let opprad = rad + Math.PI;
+    let x2 = centerX + (radius * p.cos(opprad));
+    let y2 = centerY + (radius * p.sin(opprad));
+    strokeColor += strokeColorStep;
+    if (strokeColor > 254) {
+      strokeColorStep = -1;
+    }
+    if (strokeColor < 0) {
+      strokeColorStep = +1;
     }
 
-    progress = p.min(1, progress + 0.05);
+    p.stroke(strokeColor, 60);
+    p.line(x1, y1, x2, y2);
   };
 }
 
